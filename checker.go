89def9e9e8225b9f2ba609523f609a4906b0ef6b
barsuchiha <olga.neiron@gmail.com>
2019-07-16 10:26:35 +0300

    Update checker.go

--- checker.go
+++ checker.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"regexp"
 	"net/http"
-
+	"os"
+	"time"
+	"flag"
 )
 
 type CollyData struct {
@@ -16,34 +18,56 @@ type CollyData struct {
 	anonymous int //response status code without the authorized session 
 }
 
-func main() {
+var (
+	
+	Auth_url *string = flag.String("auth_uri", "", "uri, ex. -auth_uri=/login")
+	Login_form *string = flag.String("lf", "", "Login_form, ex -lf=user_form")
+	Pass_form *string = flag.String("pf", "", "Pass_form, ex. -pf=pass_form")
+	Domain *string = flag.String("host", "", "hostname or ip-address, ex. -host=1.1.1.1")
+	User *string = flag.String("u", "", "username, ex. -u=John")
+	Pass *string = flag.String("p", "", "password, ex. -p=changeit")
+	Port *string = flag.String("Port", "", ":Port (optional, ex. -Port=:80)")
+)
 
-	crawler()
+func main() {
+	
+	filename := "WARNING_" + time.RFC3339 + ".txt"
+	file, fileErr := os.Create(filename)
+	if fileErr != nil {
+		log.Fatal(fileErr)
+		os.Exit(1)	
+	}
+	defer file.Close()
+
+	flag.Parse()
+	if *Domain == "" || *User == "" || *Pass == "" "" *Auth_url == "" || *Login_form == "" || *Pass_form == ""  {
+		fmt.Println("\x1b[91m","\x1b[1m")
+		fmt.Println("Error")
+		fmt.Println("\x1b[39m","\x1b[21m")
+		flag.Usage()
+		return
+	}  
+
+	crawler(file, *Domain, *User, *Pass, *Port)
 }
 
 //Function create Colly Collector with authorized session
 
-func crawler() {
+func crawler(file *os.File, Domain string, User string, Pass string, Port string) {
 
 	str := &CollyData{}
 
 	// create a new collector
 	logout := regexp.MustCompile(`(logout)$`)
 	c := colly.NewCollector(
-		colly.AllowedDomains("***"),
+		colly.AllowedDomains(Domain),
 		colly.UserAgent("turbobarsuchiha"),
 		colly.DisallowedURLFilters(logout),
 	)
 	
 	// authenticate
 
-	auth_url := "***"
-	login_form := "***"
-	pass_form := "***"
-	user := "***"
-	pass := "***"
-
-	err := c.Post(auth_url, map[string]string{login_form: user, pass_form: pass})
+	err := c.Post("http://"+Domain + Port + Auth_url, map[string]string{Login_form: User, Pass_form: Pass})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -67,11 +91,11 @@ func crawler() {
 		str.url = string(r.Ctx.Get("url"))
 		str.barsuchiha = r.StatusCode
 		str.anonymous = http_client(str.url) 		
-		result(str.barsuchiha, str.anonymous, str.url)
+		result(str.barsuchiha, str.anonymous, str.url, file)
 	})
 
 	// start scraping
-	c.Visit("http://***")
+	c.Visit("http://"+Domain+Port)
 
 }
 
@@ -102,9 +126,11 @@ func http_client(url string) int{
 
 //Function waits for answers with the same response status
 
-func result (barsuchiha, anonymous int, url string) {
+func result (barsuchiha, anonymous int, url string, file *os.File) {
+
 
 	if barsuchiha == anonymous {
-			fmt.Println("WARNING:", url)
+		 fmt.Fprintln(file, "WARNING:", url)
 	}
+
 }
